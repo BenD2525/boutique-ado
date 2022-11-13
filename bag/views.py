fa3991ca0ce90496b6c867a668eee064bb9e3a30
BenD2525 <bendawson2525@gmail.com>
2022-11-13 11:53:40 +0000

    Add toasts for actions

--- bag/views.py
+++ bag/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 
 def view_bag(request):
-    ''' A view to return the index page. '''
+    """ A view that renders the bag contents page """
+
     return render(request, 'bag/bag.html')
 
 
 def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -28,12 +34,14 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-
+    
 
 def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -59,6 +67,7 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
 
     try:
         size = None
@@ -67,7 +76,7 @@ def remove_from_bag(request, item_id):
         bag = request.session.get('bag', {})
 
         if size:
-            del bag[item_id]['items_by_size'[size]]
+            del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
         else:
